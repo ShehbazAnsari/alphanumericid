5c27632ee2bf4a197f91886b806e5405da31546f
Shehbazjs <reactjs5622@gmail.com>
2020-09-03 22:22:39 +0530

    Adding ReaDME

---
+++ README.md
@@ -0,0 +1,38 @@
+# What is this?
+
+Get unique alphanumericid every time for your projects
+
+# Installation 
+
+`npm i alphanumericid --save`
+
+Then...
+
+```
+import { alphanumericid } from 'alphanumericid';
+
+//For shortid
+
+alphanumericid({
+  shortid:true
+});
+
+//For longid
+
+alphanumericid({
+  longid:true
+});
+
+//Or You can Just pass a number
+
+alphanumericid(7);
+
+
+```
+
+## Options
+
+* *shortid* - _boolean_ (Defaults to false)
+* *longid* - _boolean_ (Defaults to false)
+
+
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//Generate Random AlphaNumericId
 function alphanumericid(length) {
 
   let alphaNumeric, generateNumber, randomNumber, i, alphaNumericId = ''
@@ -37,34 +36,4 @@ function alphanumericid(length) {
   }
   return alphaNumericId
 }
-
-module.exports = alphanumericid
-
-
-
-
-
-
-
-// Backup Code
-//Generate Random AlphaNumericId
-/* const getAlphaNumeric = (length) => {
-  let date, minutes, milliSeconds, uniqueTime, alphaNumeric, generateNumber, randomNumber, i
-
-  date = new Date()
-  minutes = date.getMinutes().toString()
-  milliSeconds = date.getMilliseconds().toString()
-  uniqueTime = milliSeconds + minutes
-  alphaNumeric = 'abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-  for (i = 0; i < length; i++) {
-
-    generateNumber = Math.floor(Math.random() * alphaNumeric.length + 1)
-    randomNumber = alphaNumeric.charAt(generateNumber)
-    uniqueTime += randomNumber
-
-  }
-  return uniqueTime
-}
-const uniqueAlphaNumeric = getAlphaNumeric()
-console.log(uniqueAlphaNumeric) */
+module.exports.alphanumericid = alphanumericid
